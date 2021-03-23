518adbd3a004760f9b4579bf35e06e8e51e3804d
SergX961 <50412398+SergX961@users.noreply.github.com>
2021-03-24 02:26:45 +0300

    add_lab5_2

---
+++ lab5_2/main.c
@@ -0,0 +1,379 @@
+#include "stm32f4xx.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_tim.h"
+#include "stm32f4xx_exti.h"
+#include "stm32f4xx_dma.h"
+#include "stm32f4xx_usart.h"
+#include "misc.h"
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#define COLOR_GREEN 1
+#define COLOR_RED 2
+#define COLOR_YELLOW 3
+#define COLOR_BLUE 4
+
+
+
+#define SECOND 1000
+#define PRESSED 1
+#define DELAY 128000000 / 16
+#define DELAY_LED_BLUE 250
+#define DELAY_LED_RED 650
+
+#define GPIO_PIN_GREEN GPIO_Pin_12
+#define GPIO_PIN_YELLOW GPIO_Pin_13
+#define GPIO_PIN_RED GPIO_Pin_14
+#define GPIO_PIN_BLUE GPIO_Pin_15
+
+#define GREEN_TOGGLE()  (GPIO_ToggleBits(GPIOD, GPIO_PIN_GREEN))
+#define YELLOW_TOGGLE() (GPIO_ToggleBits(GPIOD, GPIO_PIN_YELLOW))
+#define RED_TOGGLE()    (GPIO_ToggleBits(GPIOD, GPIO_PIN_RED))
+#define BLUE_TOGGLE()   (GPIO_ToggleBits(GPIOD, GPIO_PIN_BLUE))
+#define RED_ON() 			  (GPIO_SetBits(GPIOD, GPIO_PIN_RED))
+#define RED_OFF() 	    (GPIO_ResetBits(GPIOD, GPIO_PIN_RED))
+#define GREEN_ON() 		  (GPIO_SetBits(GPIOD, GPIO_PIN_GREEN))
+#define GREEN_OFF()	    (GPIO_ResetBits(GPIOD, GPIO_PIN_GREEN))
+#define YELLOW_ON() 	  (GPIO_SetBits(GPIOD, GPIO_PIN_YELLOW))
+#define YELLOW_OFF()    (GPIO_ResetBits(GPIOD, GPIO_PIN_YELLOW))
+
+#define STATE_BUTTON() (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0))
+uint8_t traffic_light_state = 0;
+uint16_t delay_count = 0,
+				 button_delay = 0,
+				 traffic_light_delay = 0;;
+
+uint32_t max_blue_delay_count = 500,
+         max_red_delay_count = 500,
+				 max_yellow_delay_count = 500,
+				 max_green_delay_count = 500;
+
+
+uint32_t blue_delay_count = 0,
+         red_delay_count = 0,
+				 yellow_delay_count = 0,
+				 green_delay_count = 0;
+
+_Bool button_delay_end = 1;
+void SysTick_Handler(void) {
+	if (delay_count > 0)
+		delay_count--;
+	
+	if (traffic_light_delay > 0)
+		traffic_light_delay --;
+	
+	if (blue_delay_count > 0) {
+		blue_delay_count--;
+		if (blue_delay_count == 0) {
+			blue_delay_count = max_blue_delay_count;
+			BLUE_TOGGLE();
+		}
+	}
+	
+	if (blue_delay_count > 0) {
+		red_delay_count--;
+		if (red_delay_count == 0) {
+			red_delay_count = max_red_delay_count;
+			RED_TOGGLE();
+		}
+	}
+
+	if (blue_delay_count > 0) {
+		yellow_delay_count--;
+		if (yellow_delay_count == 0) {
+			yellow_delay_count = max_yellow_delay_count;
+			YELLOW_TOGGLE();
+		}
+	}
+	
+	if (blue_delay_count > 0) {
+		green_delay_count--;
+		if (green_delay_count == 0) {
+			green_delay_count = max_green_delay_count;
+			GREEN_TOGGLE();
+		}
+	}
+	
+}
+
+void delay_on_tim (uint16_t ms) {
+	delay_count = ms;
+	while(delay_count);
+}
+
+
+void init_GPIO (void) {
+	GPIO_InitTypeDef GPIO_InitStructure;
+//	NVIC_InitTypeDef NVIC_InitStruct;
+//	EXTI_InitTypeDef EXTI_InitStruct;
+	
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+	
+	GPIO_InitStructure.GPIO_Pin = GPIO_PIN_GREEN;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;  
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = GPIO_PIN_YELLOW;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = GPIO_PIN_RED;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = GPIO_PIN_BLUE;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);	
+	
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_Init(GPIOA, & GPIO_InitStructure);
+//	
+//	NVIC_InitStruct.NVIC_IRQChannel = EXTI0_IRQn;
+//	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x00;
+//	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x00;
+//	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+//	NVIC_Init(&NVIC_InitStruct);
+//	
+//	EXTI_InitStruct.EXTI_Line = EXTI_Line0;
+//	EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+//	EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+//	EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
+//	EXTI_Init(&EXTI_InitStruct);
+
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+	GPIO_PinAFConfig(GPIOC,GPIO_PinSource10,GPIO_AF_UART4);
+	GPIO_PinAFConfig(GPIOC,GPIO_PinSource11,GPIO_AF_UART4);
+}
+
+void init_TIM(void) {
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+	
+	TIM_OCInitTypeDef timerPWM;
+	TIM_TimeBaseInitTypeDef base_timer;
+  
+	TIM_TimeBaseStructInit(&base_timer);
+  base_timer.TIM_Prescaler = 0;
+  base_timer.TIM_Period = 84;
+	base_timer.TIM_CounterMode = TIM_CounterMode_Up;
+	base_timer.TIM_ClockDivision = 0;
+  TIM_TimeBaseInit(TIM4, &base_timer);
+	
+	TIM_OCStructInit(&timerPWM);
+	timerPWM.TIM_OCMode = TIM_OCMode_PWM1;
+	timerPWM.TIM_OutputState = TIM_OutputState_Enable;
+	timerPWM.TIM_Pulse = 42;
+	timerPWM.TIM_OCPolarity = TIM_OCPolarity_High;
+	TIM_OC1Init(TIM4, &timerPWM);
+}
+
+
+void traffic_light_state_0 (void) {
+	traffic_light_state = 1;
+	GREEN_OFF();
+	YELLOW_OFF();
+	RED_ON();
+		traffic_light_delay = SECOND * 5;
+}
+void traffic_light_state_1 (void) {
+	traffic_light_state = 2;
+	GREEN_OFF();
+	RED_ON();
+	YELLOW_ON();
+		traffic_light_delay = SECOND * 3;
+}
+void traffic_light_state_2 (void) {
+	traffic_light_state = 3;
+	RED_OFF();
+	YELLOW_OFF();
+	GREEN_ON();
+		traffic_light_delay = SECOND * 5;
+}
+void traffic_light_state_3 (void) {
+	static uint8_t k = 0;
+	traffic_light_state = 4;
+	RED_OFF();
+	YELLOW_OFF();
+	GREEN_OFF();
+	for(uint8_t k = 0; k < 6; k++) {
+		if (traffic_light_state != 4)
+			return;
+		GREEN_TOGGLE();
+		traffic_light_delay = SECOND;
+	}
+}
+void traffic_light_state_4 (void) {
+	traffic_light_state = 0;
+	RED_OFF();
+	GREEN_OFF();
+	YELLOW_ON();
+		traffic_light_delay = SECOND * 3;
+}
+
+void traffic_light (void) {
+	switch (traffic_light_state){
+		case 0: traffic_light_state_0();
+			break;
+		case 1: traffic_light_state_1();
+			break;
+		case 2: traffic_light_state_2();
+			break;
+		case 3: traffic_light_state_4();
+			break;
+	}
+}
+
+void EXTI0_IRQHandler (void) {
+	EXTI_ClearITPendingBit(EXTI_Line0);
+	button_delay = 50;
+	button_delay_end = 0;
+}
+
+DMA_InitTypeDef dma;
+USART_InitTypeDef uart4;
+#define DMA_BUFFER_SIZE 20
+uint8_t dataBuffer[DMA_BUFFER_SIZE] = {0};
+
+void init_UART (void) {
+	
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART4, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA1, ENABLE);
+	
+	USART_StructInit(&uart4);
+	uart4.USART_BaudRate = 115200;
+	uart4.USART_WordLength = USART_WordLength_8b;
+	uart4.USART_StopBits = USART_StopBits_1;
+	uart4.USART_Parity = USART_Parity_No;
+
+	USART_Init(UART4, &uart4);
+}
+
+#define MAX_RX_COUNT 30
+uint8_t rx_uart_buffer [MAX_RX_COUNT + 1] = {0};
+_Bool rx_allow = true,
+      command_recieve = false;
+
+void UART4_IRQHandler (void) {
+	static uint8_t symbols_count = 0;
+	uint8_t rx_symbol = USART_ReceiveData(UART4);
+	USART_ClearITPendingBit(UART4, USART_IT_RXNE);
+	
+	if (!rx_allow)
+		return;
+	
+	if ((rx_symbol != '\r') & (symbols_count < MAX_RX_COUNT)) {
+	rx_uart_buffer[symbols_count] = rx_symbol;
+	symbols_count++;
+	} else {
+		rx_allow = false;
+		command_recieve = true;
+		symbols_count = 0;
+	}
+	
+}
+void transmit_str (uint8_t * str) {
+	if (str == 0)
+		return;
+	
+//	USART_ITConfig(UART4, USART_IT_RXNE, DISABLE);
+	
+	uint16_t len = strlen((const char *)str);
+	for (uint16_t k = 0; k < len; k++) {
+		USART_ClearFlag(UART4, USART_FLAG_TC);
+		USART_SendData(UART4, str[k]);
+		while (USART_GetFlagStatus(UART4, USART_FLAG_TC) != SET);
+	}
+	USART_ClearFlag(UART4, USART_FLAG_TC);
+//	USART_ITConfig(UART4, USART_IT_RXNE, ENABLE);
+//	USART_ClearITPendingBit(UART4, USART_IT_RXNE);
+}
+
+void return_error (void) {
+	transmit_str("Error occured\n");
+}
+
+uint32_t conv_led_time_to_digit (const char * str) {
+	if (isdigit(*str)) {
+			 return atoi((const char *)str);
+		} else {
+			return 0;
+		}
+}
+
+void set_led_time (uint8_t color, uint32_t time) {
+	if (color == COLOR_GREEN) {
+		max_green_delay_count = time;
+		green_delay_count = 1;
+	} else if (color == COLOR_RED) {
+		max_red_delay_count = time;
+		red_delay_count = 1;
+	} else if (color == COLOR_YELLOW) {
+		max_yellow_delay_count = time;
+		yellow_delay_count = 1;
+	} else if (color == COLOR_BLUE) {
+		max_blue_delay_count = time;
+		blue_delay_count = 1;
+	}
+}
+
+void parse_command (void) {
+	uint8_t * command_str = rx_uart_buffer;
+	uint8_t command_len = strlen((const char *)command_str),
+		      color = 0;
+	uint32_t sf_state = 0;
+	
+	if (!strncmp((const char *)command_str, "set_state", strlen("set_state"))) {
+		command_str += strlen("set_state") + 1;
+		sf_state = conv_led_time_to_digit ((const char *)command_str);
+		
+		if ((sf_state > 0) & (sf_state < 4)) {
+			traffic_light_delay = 0;
+			traffic_light_state = sf_state - 1;
+		} else {
+			return_error();
+			return;
+		}
+	
+	} else {
+		return_error();
+		return;
+	}
+		
+}
+
+
+int main (void) {
+	SysTick_Config(SystemCoreClock/1000);
+	init_GPIO();
+	init_UART();
+	USART_Cmd(UART4, ENABLE);
+	NVIC_EnableIRQ(UART4_IRQn);
+	USART_ITConfig(UART4, USART_IT_RXNE, ENABLE);
+	
+//	transmit_str("System started\n");
+	
+	while(1) {
+		if (command_recieve) {
+			parse_command();
+			command_recieve = false;
+			rx_allow = true;
+		}
+		
+		if (traffic_light_delay == 0)
+			traffic_light();
+	}
+}
